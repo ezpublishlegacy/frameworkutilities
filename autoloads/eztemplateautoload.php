3897cf32b16f95edd47d0582a19e98b5ad86211d
Michael <michael@thinkcreative.com>
2012-04-06 11:29:09 -0400

    create "array_remove" php function

--- autoloads/eztemplateautoload.php
+++ autoloads/eztemplateautoload.php
@@ -1,5 +1,11 @@
 <?php
 
+if(!function_exists('array_remove')){
+	function array_remove(&$array, $value, $key=false){
+		array_splice($array, array_search($value, $array), 1);
+	}
+}
+
 $eZTemplateOperatorArray=SiteUtils::TemplateOperators('extension/frameworkutilities');
 
 ?>
