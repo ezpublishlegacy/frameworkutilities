1585b210785c554e0f24c442d83edfb2d733ce31
Michael <michael@thinkcreative.com>
2012-05-04 09:10:18 -0400

    move template operator autoload array setting

--- autoloads/eztemplateautoload.php
+++ autoloads/eztemplateautoload.php
@@ -1,11 +1,11 @@
 <?php
 
+$eZTemplateOperatorArray = SiteUtils::TemplateOperators('extension/frameworkutilities');
+
 if(!function_exists('array_remove')){
 	function array_remove(&$array, $value, $key=false){
 		array_splice($array, array_search($value, $array), 1);
 	}
 }
-
-$eZTemplateOperatorArray=SiteUtils::TemplateOperators('extension/frameworkutilities');
 
 ?>
