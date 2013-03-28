c097bd03567201539a64eedcd4c88d4b3edd14c6
David Broadfoot <david@thinkcreative.com>
2013-03-28 16:29:13 -0400

    turn off cdn in edit

--- operators/productiononlyoperator.php
+++ operators/productiononlyoperator.php
@@ -23,10 +23,11 @@ class productiononlyOperator
 	}
 
 	function modify(&$tpl, &$operatorName, &$operatorParameters, &$rootNamespace, &$currentNamespace, &$operatorValue, &$namedParameters){
+		$ini = eZINI::instance();
 		if (file_exists("/mnt/ebs/iamproduction.txt") && strpos($_SERVER[HTTP_HOST], 'thinkcreativeinternal') === false) {
 			$GLOBALS['eZDebugEnabled'] = false;
+			if (strpos($_SERVER['REQUEST_URI'], 'content/edit') > -1) $ini->setVariable('OutputSettings', 'OutputFilterName', '');
 		} else {
-			$ini = eZINI::instance();
 			$ini->setVariable('OutputSettings', 'OutputFilterName', '');
 		}
 		return true;
